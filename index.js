a39fb7b3397b1485e5a12d4762d3987f445b81fb
zjan3 <159425626+zjan3@users.noreply.github.com>
2024-02-18 20:45:48 +0430

    created real time

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function refresh(response){
   
   let theSpeed=document.querySelector("#speed");
   theSpeed.innerHTML=`${response.data.wind.speed}km/h`;
+
+  
 }
+
 
 
 
@@ -43,3 +46,35 @@ let form=document.querySelector("#input-search");
 form.addEventListener("submit", search);
 
 searchCity("Kabul");
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let currentDateELement = document.querySelector("#time");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
